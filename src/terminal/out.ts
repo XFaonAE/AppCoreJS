515b5e41b1a1c6921213e520e9deb3796b657b57
XFaon <xfaonae@protonmail.com>
2021-12-23 16:45:57 -0500

    Added basics for source

---
+++ src/terminal/out.ts
@@ -0,0 +1,17 @@
+/**
+ * Log a formatted message into the console
+ * @param tag Tag prefix
+ * @param color Tag color
+ * @param message Message
+ */
+function log(tag: string, color: string, message: string) {
+
+}
+
+/**
+ * Log an info message to the console
+ * @param message Message
+ */
+export function info(message: string) {
+
+}
